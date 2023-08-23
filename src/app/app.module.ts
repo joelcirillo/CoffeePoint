1ff6a7d6f571a0a36abb3ff684c624072acdc6af
joelcirillo <joelcirillo2005@gmail.com>
2023-08-23 08:44:48 -0300

    se vincula firebase coon el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//se importa el environment
+import { environment } from "src/environments/environment";
+//se importa modulos de firebase
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth"; 
+import { AngularFireStorageModule } from "@angular/fire/compat/storage";
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //llamando modulos firebase,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
