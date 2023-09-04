2504f7c7cef52b72b340132268d12757af464410
joelcirillo <joelcirillo2005@gmail.com>
2023-09-04 08:44:11 -0300

    se intenta agregar el mapa

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  constructor() {}
+
+  ngOnInit(): void {
+    const myMap = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+    }).addTo(myMap);
+  }
 
 }
