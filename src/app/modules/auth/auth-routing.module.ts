5ff22ba61eaeec59fa52f3c630576ab15c224693
valen117 <valenbello1234@gmail.com>
2023-09-16 11:21:49 -0300

    haciendo el ruteo del register

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {
     path:'login',component:LoginComponent
+  },
+  {
+    path:'register',component:RegisterComponent
   },
 ];
 
