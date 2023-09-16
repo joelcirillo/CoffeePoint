3dcfa54e4669d75e91a0cfe032548e95916e5f08
vanicalabrese <vanicalabrese011@gmail.com>
2023-09-16 13:42:26 -0300

    realicé el cuadro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './reseña/pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:"*",component:PagesComponent
+  }
+
+
+ 
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
