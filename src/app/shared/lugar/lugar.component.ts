77218979ab7802348a9c6e9d8a1690957485e3ee
joelcirillo <joelcirillo2005@gmail.com>
2023-09-17 13:44:13 -0300

    se intentan resolver conflictos de git

--- src/app/shared/lugar/lugar.component.ts
+++ src/app/shared/lugar/lugar.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 58f6084a9eade26ddec63a38ea7e4c5e6962c4dd
 @Component({
   selector: 'app-lugar',
   templateUrl: './lugar.component.html',
