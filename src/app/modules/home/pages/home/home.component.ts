51ee74594bbd6c392c49169e735b529f224952be
joelcirillo <joelcirillo2005@gmail.com>
2023-10-18 11:03:13 -0300

    se agrega tabla para ingresar nuevas cafeterias

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
