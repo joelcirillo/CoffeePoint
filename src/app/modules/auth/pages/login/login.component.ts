4d831c1b6bd6cb20c3acfc93d33a6c291144c2a0
valen117 <valenbello1234@gmail.com>
2023-10-30 22:05:42 -0300

    creando las carpetas services

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service'; // función iniciar sesión
+import { FirestoreService } from 'src/app/shared/services/firestore.service'; // nos trae los datos
+import { Usuario } from 'src/app/models/usuario'; // interfaz usuario
+import { Router } from '@angular/router'; // navegación
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  hide = true;
 
+  usuarios: Usuario = {
+    uid: '',
+    nombre: '',
+    email: '',
+    rol: '',
+    contrasena: ''
+  }
+
+  constructor(
+    public servicioAuth: AuthService,
+    public firestore: FirestoreService,
+    public router: Router
+  ){}
+
+  async iniciar(){
+    const credenciales = {
+      email: this.usuarios.email,
+      contrasena: this.usuarios.contrasena
+    }
+
+    const res = await this.servicioAuth.iniciarSesion(credenciales.email, credenciales.contrasena)
+    .then(res => {
+      alert("Se ha logueado con éxito :D");
+
+      this.router.navigate(['/inicio']);
+    })
+    .catch(error => {
+      alert("Hubo un error al iniciar sesión :( \n +" + error);
+    })
+  }
+
+  // función para CERRAR SESIÓN
+  async salir(){
+    const res = await this.servicioAuth.cerrarSesion()
+    .then(res => {
+      alert("Ha cerrado sesión con éxito.");
+      console.log(res);
+
+      this.router.navigate(['/inicio']);
+    })
+  }
 }
---
+++ src/app/shared/services/firestore.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Usuario } from 'src/app/models/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+  private usuariosCollection: AngularFirestoreCollection<Usuario>
+
+  // dentro de los parametros la BD
+  constructor(private database: AngularFirestore) {
+    // refenciamos colección de la BD
+    this.usuariosCollection = this.database.collection<Usuario>('usuarios')
+  }
+
+  agregarUsuario(usuario: Usuario, id: string) {
+    // RESOLVE: promesa resulta -> similar al then
+    // REJECT: promesa rechazada -> similar al catch
+    return new Promise(async (resolve, reject) => {
+      try {
+        usuario.uid = id;
+
+        const resultado = await this.usuariosCollection.doc(id).set(usuario)
+        // muestra el resultado sin problema
+        resolve(resultado)
+      }catch(error){
+        // en caso de que ocurra un error
+        reject(error)
+      }
+    })
+  }
+}
