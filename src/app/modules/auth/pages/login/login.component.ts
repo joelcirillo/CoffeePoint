ff8f7335ca35faed916cfd9f9c27b5ad3885bf6f
“valen117” <valenbello1234@gmail.com>
2023-10-31 11:44:02 -0300

    colocando los ngModels

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../services/auth.service';
-import { Usuario } from 'src/app/models/usuario';
-import { FirestoreService } from 'src/app/shared/services/firestore.service';
-import { Router } from '@angular/router';
-
+import { AuthService } from '../services/auth.service'; // función iniciar sesión
+import { FirestoreService } from 'src/app/shared/services/firestore.service'; // nos trae los datos
+import { Usuario } from 'src/app/models/usuario'; // interfaz usuario
+import { Router } from '@angular/router'; // navegación
 
 @Component({
   selector: 'app-login',
@@ -11,36 +10,48 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  hide = true; //para el input
+  hide = true;
 
-  usuarios: Usuario={
+  usuarios: Usuario = {
     uid: '',
     nombre: '',
-    email:'',
-    rol:'',
-    contrasena:'',
+    email: '',
+    rol: '',
+    contrasena: ''
   }
 
-  uid='';
+  constructor(
+    public servicioAuth: AuthService,
+    public firestore: FirestoreService,
+    public router: Router
+  ){}
 
+  async iniciar(){
+    const credenciales = {
+      email: this.usuarios.email,
+      contrasena: this.usuarios.contrasena
+    }
 
- //creamos una nueva coleccion para usuarios
-  coleccionUsuarios: Usuario[] = [];
+    const res = await this.servicioAuth.iniciarSesion(credenciales.email, credenciales.contrasena)
+    .then(res => {
+      alert("Se ha logueado con éxito :D");
 
+      this.router.navigate(['/inicio']);
+    })
+    .catch(error => {
+      alert("Hubo un error al iniciar sesión :( \n +" + error);
+    })
+  }
 
- // servicioAuth referencia a nuestro servicio Auth
- constructor(
-  public servicioAuth: AuthService, 
-  public servicioFirestore: FirestoreService,
-  public router: Router
-  ) {
-}
+  // función para CERRAR SESIÓN
+  async salir(){
+    const res = await this.servicioAuth.cerrarSesion()
+    .then(res => {
+      alert("Ha cerrado sesión con éxito.");
+      console.log(res);
 
-//tomamos nuevos registros y mostramos los resultados 
-async registrarse (){
-  const credenciales ={
-    email: this.usuarios.contrasena
+      this.router.navigate(['/inicio']);
+    })
   }
 }
 
-}
