f036973a9bec556d64df30e1d02afd365fa6591a
“valen117” <valenbello1234@gmail.com>
2023-10-31 09:24:39 -0300

    avanzando con el login component

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Usuario } from 'src/app/models/usuario';
+import { FirestoreService } from 'src/app/shared/services/firestore.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +11,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  hide = true; //para el input
+
+  usuarios: Usuario={
+    uid: '',
+    nombre: '',
+    email:'',
+    rol:'',
+    contrasena:'',
+  }
+
+  uid='';
+
+
+ //creamos una nueva coleccion para usuarios
+  coleccionUsuarios: Usuario[] = [];
+
+
+ // servicioAuth referencia a nuestro servicio Auth
+ constructor(
+  public servicioAuth: AuthService, 
+  public servicioFirestore: FirestoreService,
+  public router: Router
+  ) {
+}
+
+//tomamos nuevos registros y mostramos los resultados 
+async registrarse (){
+  const credenciales ={
+    email: this.usuarios.contrasena
+  }
+}
 
 }
